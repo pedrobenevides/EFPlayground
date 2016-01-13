6138372ccd52d48f518e2097580d1e7d75ba3fdc
Pedro Benevides <pedrobenevidesb@gmail.com>
2016-01-13 11:28:03 -0300

    Adicionando Console Table

--- src/EFStudy.Output/Program.cs
+++ src/EFStudy.Output/Program.cs
@@ -4,6 +4,7 @@ using EFStudy.Core.Entities;
 using EFStudy.Infra.Data;
 using EFStudy.Infra.Data.Configuration;
 using EFStudy.Infra.Data.Repositories;
+using ConsoleTables.Core;
 
 namespace EFStudy.Output
 {
@@ -21,7 +22,16 @@ namespace EFStudy.Output
             var clients = clientRepository.GetAll();
             var list = clients.ToList();
             var isInMemory = DBExtensions.IsQueryableInMemory(clients);
+
+            var table = new ConsoleTable("Nome");
+
+            foreach (var client in list)
+            {
+                table.AddRow(client.Name);
+            }
+
             
+            table.Write();
             Console.ReadKey();
         }
     }
